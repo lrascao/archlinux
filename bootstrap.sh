5ce28b523152d1c66e2015a8fa1c2462665c9d77
Luis Rascao <luis.rascao@miniclip.com>
2020-11-01 01:24:05 +0000

    Improve initial instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,33 @@
 HOSTNAME=monad
 USER=luis
 
-# partition the disk:
-#   https://wiki.archlinux.org/index.php/GPT_fdisk#:~:text=GPT%20fdisk%E2%80%94consisting%20of%20the,Record%20(MBR)%20partition%20tables.
+# 1. basic setup
+#
+# $ loadkeys pt-latin1
 
-pacstrap /mnt base linux
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt/
+# 2. partition the disk:
+#   https://wiki.archlinux.org/index.php/GPT_fdisk#Create_a_partition_table_and_partitions
+#
+#   Create 3 partitions:
+#       1. /boot EFI system partition, 300M, [ef00]
+#       2. /root Linux x86-64 root partition, 35G, [8304]
+#       3. /home Linux partition, rest of disk, [8302]
+# $ gdisk /dev/<disk>  
+#
+# $ mkfs.fat /dev/<sd>1
+# $ mkfs.ext4 /dev/<sd>2
+# $ mkfs.ext4 /dev/<sd>3
+#
+# $ mount /dev/<sd>2 /mnt
+# $ mkdir /mnt/efi
+# $ mount /dev/<sd>1 /mnt/efi
+
+
+# 3. fetch bootstrap shell script from github
+#
+# $ pacstrap /mnt base linux git
+# $ genfstab -U /mnt >> /mnt/etc/fstab
+# $ arch-chroot /mnt/
 
 LANG=C perl -i -pe 's/#(en_US.UTF)/$1/' /etc/locale.gen
 LANG=C perl -i -pe 's/#(pt_PT.UTF)/$1/' /etc/locale.gen
@@ -22,15 +43,18 @@ ln -sf /usr/share/zoneinfo/Europe/Lisbon /etc/localtime
 
 echo $HOSTNAME > /etc/hostname
 
-pacman -S dialog refind-efi
+pacman -S sudo git binutils gdisk dialog refind-efi dosfstools
 mkdir -p /efi/EFI/Boot
 cp /usr/share/refind/refind_x64.efi /efi/EFI/Boot/bootx64.efi
+cp -r /usr/share/refind/drivers_x64/ /efi/EFI/Boot/
 echo 'extra_kernel_version_strings linux,linux-hardened,linux-lts,linux-zen,linux-git;' > /efi/EFI/Boot/refind.conf
 echo 'fold_linux_kernels false' >> /efi/EFI/Boot/refind.conf
 echo 'default_selection "linux from"' >> /efi/EFI/Boot/refind.conf
+echo 'timeout 5' >> /efi/EFI/Boot/refind.conf
 
 useradd -m -G wheel -s /bin/bash $USER
 passwd $USER
-pacman -S sudo git binutils gdisk dosfstools
 perl -i -pe 's/# (%wheel ALL=\(ALL\) ALL)/$1/' /etc/sudoers
+
+echo "run: 'fatlabel /dev/<sd>1 ArchLinux'"
 
